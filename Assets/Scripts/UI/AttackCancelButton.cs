8c93d2dca1fb01e5e9bcf9c6b407184171091338
kg8812 <kg88124@gmail.com>
2023-04-25 18:24:29 +0900

    UI 추가
    UI 이미지 적용

---
+++ Assets/Scripts/UI/AttackCancelButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AttackCancelButton : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        TurnManager.Instance.CancelSkill();
+    }
+}
