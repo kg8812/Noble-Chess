23485d3a3255a08a475a7b5238b78b3334f79554
kg8812 <kg88124@gmail.com>
2023-04-30 20:07:40 +0900

    캔버스 스크립트 추가 및 UI 수정
    캔버스 해상도관련 스크립트 추가
    UI 앵커 추가 수정

---
+++ Assets/Scripts/UI/CanvasUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasUI : MonoBehaviour
+{
+    CanvasScaler scaler;
+
+    private void Start()
+    {       
+        scaler = GetComponent<CanvasScaler>();
+    }
+    public void ChangeScale()
+    {
+        int width = Screen.width;
+        int height = Screen.height;
+        scaler.referenceResolution = new Vector2(width, height);
+    }
+}
