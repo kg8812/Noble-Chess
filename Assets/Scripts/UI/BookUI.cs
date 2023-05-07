3e7c972da94738619fa12ec476296acd881cc365
kg8812 <kg88124@gmail.com>
2023-05-07 16:54:59 +0900

    인벤토리 수정

--- Assets/Scripts/UI/BookUI.cs
+++ Assets/Scripts/UI/BookUI.cs
@@ -5,24 +5,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class BookUI : MonoBehaviour
-{
-    GridLayoutGroup grid;
-    RectTransform rect;
-
+{  
     Image[] images = new Image[16];
     Queue<Skill> skillQueue = new Queue<Skill>();
     public GameObject reservedSkill;
+
     void Start()
-    {        
-        grid = GetComponent<GridLayoutGroup>();
-        rect = GetComponent<RectTransform>();
-
-        float width = rect.rect.width;
-        float height = rect.rect.height;
-
-        grid.cellSize = new Vector2(width / 9, height * 0.475f);
-        grid.spacing = new Vector2(width / 63, height * 0.05f);       
-
+    {                      
         for(int i = 0; i < 16; i++)
         {
             Image image = Instantiate(reservedSkill,transform).GetComponent<Image>();                        
